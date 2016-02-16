ec36bdae94f84548df0b78d0cb9068acb9a9dc88
eehhgg <eehhgg@hotmail.com>
2016-02-16 15:40:09 -0600

    Optimized 5_MinAvgTwoSlice

--- lessons/5_MinAvgTwoSlice.php
+++ lessons/5_MinAvgTwoSlice.php
@@ -2,26 +2,29 @@
 
 function solution($a) {
   $n = count($a);
-  // find the minimal slice of length 2
+  if ($n == 2) { return 0; }
+  // find the minimal slices of length 2 and 3
   $minSum2 = $a[0] + $a[1];
   $minIndex2 = 0;
-  for ($i = 1; $i <= $n-2; $i++) {
+  $minSum3 = $a[0] + $a[1] + $a[2];
+  $minIndex3 = 0;
+  for ($i = 1; $i <= $n-3; $i++) {
     $sum = $a[$i] + $a[$i+1];
     if ($sum < $minSum2) {
       $minSum2 = $sum;
       $minIndex2 = $i;
     }
-  }
-  if ($n == 2) { return $minIndex2; }
-  // find the minimal slice of length 3
-  $minSum3 = $a[0] + $a[1] + $a[2];
-  $minIndex3 = 0;
-  for ($i = 1; $i <= $n-3; $i++) {
-    $sum = $a[$i] + $a[$i+1] + $a[$i+2];
+    $sum += $a[$i+2];
     if ($sum < $minSum3) {
       $minSum3 = $sum;
       $minIndex3 = $i;
     }
+  }
+  $i = $n-2;
+  $sum = $a[$i] + $a[$i+1];
+  if ($sum < $minSum2) {
+    $minSum2 = $sum;
+    $minIndex2 = $i;
   }
   // return the starting position of the minimal slice
   // minSum2/2 < minSum3/3 === 3*minSum2 < 2*minSum3
